4103291bf7c0a0715f304c486143237a44052ec0
Yunior Montaner <ymh.tanatos@gmail.com>
2020-07-28 12:44:43 -0400

    remove meta tags on app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,13 +15,5 @@ export class AppComponent implements OnInit {
   constructor(private title: Title, private meta: Meta) {}
 
   ngOnInit(): void {
-    this.title.setTitle(this.data.name);
-    this.meta.addTags([
-      { name: 'twitter:card', content: 'summary' },
-      { name: 'og:url', content: '/' },
-      { name: 'og:title', content: this.data.name },
-      { name: 'og:description', content: this.data.description },
-      { name: 'og:image', content: this.data.image },
-    ]);
   }
 }
